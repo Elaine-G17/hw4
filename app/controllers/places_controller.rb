51a50c7bae58837ad6732389b242a49dcecfb681
Elaine-G17 <yilingu2023@u.northwestern.edu>
2023-03-09 06:14:47 +0000

    0309

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,13 +4,14 @@ class PlacesController < ApplicationController
     @places = Place.all
   end
 
-  def show
-    @place = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ "place_id" => @place["id"] })
-  end
-
   def new
     @place = Place.new
+  end
+
+  def show
+    @place = Place.find_by({ "id" => params["id"] })
+    @posts = Post.where({ "place_id" =>@place["id"]})
+    @user_posts = Post.where({ "user_id" => @current_user["id"],"place_id" =>@place["id"] })
   end
 
   def create
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if @user
       if BCrypt::Password.new(@user["password"]) == params["password"]
         session["user_id"] = @user["id"]
-        flash["notice"] = "Hello."
+        flash["notice"] = "Hello"
         redirect_to "/places"
       else
         flash["notice"] = "Please Try Again."
@@ -21,7 +21,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session["user_id"] = nil
-    flash["notice"] = "Goodbye."
+    flash["notice"] = "Goodbye"
     redirect_to "/login"
   end
 end
